f676091477c62e40d402e5a5b003c3ecec33ace3
EdarielT <47457123+EdarielT@users.noreply.github.com>
2021-06-30 04:12:25 +0200

    Add files via upload
    Just a plate, w/o real content.

---
+++ game.js
@@ -0,0 +1,93 @@
+const question = document.getElementById ('question');
+const choices = Array.from(document.getElementsByClassName ('choice-text'));
+const quesCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+
+
+let currentQues = {};
+let acceptChoice = false;
+let score = 0;
+let quesCounter = 0;
+let availableQues = [];
+
+let questions = [];
+
+fetch("questions.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions;
+        
+        startGame();
+    });
+
+//constants
+
+const correctPoints = 5; //how many points user gets for a correct answer
+const quesMax = 3; //how many questions does it take to end the game
+
+startGame = () => {
+    quesCounter = 0;
+    score = 0;
+    availableQues = [...questions];
+    getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
+};
+
+getNewQuestion = () => {
+    if(availableQues === 0 || quesCounter >= quesMax) {
+        localStorage.setItem('mostRecentScore', score);
+        //end of the game
+        return window.location.assign('/end.html');
+    }
+    quesCounter++;
+    quesCounterText.innerText = `${quesCounter}/${quesMax}`;
+    const quesIndex = Math.floor(Math.random()*availableQues.length);
+    currentQues = availableQues[quesIndex];
+    question.innerText = currentQues.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQues['choice' + number];  //to get the needed set of choices for each question
+    });
+
+    availableQues.splice(quesIndex, 1);  //to get rid of the question that we just answered
+
+    acceptChoice = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptChoice) return;
+
+        acceptChoice = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        let classToApply = 'incorrect';
+        if(selectedAnswer == currentQues.answer){
+            classToApply = 'correct';
+        };
+
+        if(classToApply === 'correct') {
+            incrementScore(correctPoints);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+        
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000); //wait 1sec before removing the class and going to the next ques
+    });
+
+});
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
